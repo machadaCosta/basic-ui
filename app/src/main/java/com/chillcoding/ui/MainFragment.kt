85db5d154e64b37b361981036116c01217db56c8
macha <macha.dacosta@gmail.com>
2020-12-15 22:59:38 +0100

    Add View Binding

--- app/src/main/java/com/chillcoding/ui/MainFragment.kt
+++ app/src/main/java/com/chillcoding/ui/MainFragment.kt
@@ -7,25 +7,29 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.chillcoding.ui.databinding.FragmentMainBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.mainBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_MainFragment_to_SecondFragment)
         }
     }
 }
