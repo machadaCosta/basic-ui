c1bd06e63d2010368a609d825ad357d89e1aa739
macha <macha.dacosta@gmail.com>
2020-12-17 10:39:45 +0100

    Add Alert Dialog with splitties

--- app/src/main/java/com/chillcoding/ui/MainActivity.kt
+++ app/src/main/java/com/chillcoding/ui/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.chillcoding.ui
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import splitties.toast.toast
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/chillcoding/ui/MainFragment.kt
+++ app/src/main/java/com/chillcoding/ui/MainFragment.kt
@@ -1,13 +1,14 @@
 package com.chillcoding.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.chillcoding.ui.databinding.FragmentMainBinding
+import splitties.alertdialog.appcompat.*
+import splitties.alertdialog.material.materialAlertDialog
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -17,8 +18,8 @@ class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentMainBinding.inflate(layoutInflater)
@@ -28,8 +29,19 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.mainAlertBtn.setOnClickListener { showAlertDialog() }
         binding.mainBtn.setOnClickListener {
             findNavController().navigate(R.id.action_MainFragment_to_SecondFragment)
         }
+    }
+
+    private fun showAlertDialog() {
+        context?.materialAlertDialog {
+            messageResource = R.string.text_alert_dialog
+            okButton { showAlertDialog() }
+            cancelButton()
+        }?.onShow {
+            positiveButton.setText(R.string.show)
+        }?.show()
     }
 }
