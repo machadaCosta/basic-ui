a30c16bfe0a8f70907af0f8413b9f785bd3de371
macha <macha.dacosta@gmail.com>
2020-12-17 17:23:06 +0100

    Add sharing

--- app/src/main/java/com/chillcoding/ui/MainActivity.kt
+++ app/src/main/java/com/chillcoding/ui/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> share(R.string.text_share, R.string.title_share)
             else -> super.onOptionsItemSelected(item)
         }
     }
