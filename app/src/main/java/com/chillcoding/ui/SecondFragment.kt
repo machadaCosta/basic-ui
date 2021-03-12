b60e5d949a1dbfb8b79612781e485af78eae7c1c
macha <macha.dacosta@gmail.com>
2021-03-12 14:51:04 +0100

    Add Binding on SecondFragment

--- app/src/main/java/com/chillcoding/ui/SecondFragment.kt
+++ app/src/main/java/com/chillcoding/ui/SecondFragment.kt
@@ -1,30 +1,33 @@
 package com.chillcoding.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.chillcoding.ui.databinding.FragmentSecondBinding
+import com.chillcoding.ui.ui.login.LoginActivity
+import splitties.activities.start
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class SecondFragment : Fragment() {
 
+    private lateinit var binding: FragmentSecondBinding
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        binding = FragmentSecondBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+        binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_MainFragment)
         }
     }
---
+++ app/src/main/java/com/chillcoding/ui/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.chillcoding.ui.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
